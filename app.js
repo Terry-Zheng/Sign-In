2413cf23f73a0dc0032f4d3ca47d828e828f3c89
Terry <1302983863@qq.com>
2020-01-04 18:27:45 +0800

    20/01/04 v1.1.0 已添加密码/重复密码，下一步：加入持久化

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
 
 var app = express();
 
@@ -17,10 +18,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    store: new FileStore(),
+    resave: false,
+    saveUninitialized: false,
+    secret: 'Web Homework11 Sign Up'
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var validator = require('../public/javascripts/validator');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var registed_Users = {};
+
+/* RegistPage routes. */
+router.get('/regist', function(req, res, next) {
+  res.render('RegistPage', {title: '注册', user:{}});
+});
+
+router.post('/regist', function(req, res, next) {
+  var newUser = req.body;
+  // console.log(newUser);
+  try {
+    checkUser(newUser);
+    req.session.user = registed_Users[newUser["username"]] = newUser;
+    res.redirect('/detail');
+  } catch (err) {
+    res.render('RegistPage', {title: '注册', user: newUser, error: err.message})
+  }
+});
+
+router.all('*', function(req, res, next){
+  req.session.user ? next() : res.redirect('/login');
+});
+
+/* DetailPage routes. */
+router.get('/detail', function(req, res, next) {
+  res.render('DetailPage', {title: '详情', user: req.session.user});
 });
 
 module.exports = router;
+
+// Check user validation
+function checkUser(user){
+  var errMsg = [];
+
+  for (var info in user) {
+    if (!validator.checkInfoFormat(info, user[info]))
+      errMsg.push(validator.getErrorMessage(info));
+    if (info == "userpassword" || info == "userrepeat") continue;
+    if (!validator.checkAttrValueUnique(registed_Users, user, info))
+      errMsg.push("This " + info + " has been used, please input another " + info);
+  }
+
+  if (errMsg.length > 0) throw new Error(errMsg.join('<br/>'));
+}
